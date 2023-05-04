d1659237f36d2ddef1eac9f09a1b926462f99e4c
prasannaallada <prasannaallada9@gmail.com>
2023-05-04 06:31:30 +0000

    editable text input

---
+++ src/App.js
@@ -0,0 +1,10 @@
+// Published url: praeditxt.ccbp.tech
+
+import AppItem from './components/AppItem'
+
+import './App.css'
+
+// Replace your code here
+const App = () => <AppItem />
+
+export default App
